f16795e87f6711f5c979d6219dcaac1c4c11de01
Tommasina <tommasina1@gmail.com>
2016-09-20 11:01:39 -0600

    stores are rendering on the page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,8 @@ class SearchController <ApplicationController
   
   def index
     @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6")
-    JSON.parse(@response.body)["stores"].each do |store|
+    @total = JSON.parse(@response.body)["total"]
+    @stores = JSON.parse(@response.body)["stores"].each do |store|
       OpenStruct.new(store)
     end
   end
