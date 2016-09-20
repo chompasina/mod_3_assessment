7c7c7d328c1d167c777efce48bc7a5f992789818
Tommasina <tommasina1@gmail.com>
2016-09-20 10:47:33 -0600

    working on view for results from store search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,11 @@
 class SearchController <ApplicationController
+  respond_to :json
   
   def index
+    @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6")
+    JSON.parse(@response.body)["stores"].each do |store|
+      OpenStruct.new(store)
+    end
   end
   
 end
