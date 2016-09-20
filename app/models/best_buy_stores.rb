51ea6fcf0d0902705fcd2249ec772867ed5690ec
Tommasina <tommasina1@gmail.com>
2016-09-20 12:04:43 -0600

    Still was working on refactoring the service but all tests still passing

--- app/models/best_buy_stores.rb
+++ app/models/best_buy_stores.rb
@@ -2,16 +2,7 @@ class BestBuyStores <OpenStruct
   attr_reader :total
   
   def self.search_by_zip(zip)
-    service.search_by_zip(zip).map do |store|
-      new(store)
-    end
-    
-    @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=1")
-    @response2 = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=2")
-    @first_stores = JSON.parse(@response.body)["stores"]
-    @second_stores = JSON.parse(@response2.body)["stores"]
-    @stores = @first_stores + @second_stores
-    @stores.take(15).each do |store|
+    service.search_by_zip(zip).take(15).map do |store|
       new(store)
     end
   end
@@ -21,7 +12,6 @@ class BestBuyStores <OpenStruct
   end
   
   def self.total(zip)
-    @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=1")
-    @total = JSON.parse(@response.body)["total"]
+    service.total(zip)
   end
 end
