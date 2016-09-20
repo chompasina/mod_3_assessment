956b00aec8d8825985bc79cdd598f60e13c07081
Tommasina <tommasina1@gmail.com>
2016-09-20 11:46:33 -0600

    working on refactoring the poro to a service

--- app/models/best_buy_stores.rb
+++ app/models/best_buy_stores.rb
@@ -1,15 +1,27 @@
-class BestBuyStores
+class BestBuyStores <OpenStruct
+  attr_reader :total
   
-  def self.search_by_zip(zip, distance)
-    @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=1")
-    require "pry"; binding.pry
-    @response2 = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=2")
-    @total = JSON.parse(@response.body)["total"]
+  def self.search_by_zip(zip)
+    service.search_by_zip(zip).map do |store|
+      new(store)
+    end
+    
+    @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=1")
+    @response2 = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=2")
     @first_stores = JSON.parse(@response.body)["stores"]
     @second_stores = JSON.parse(@response2.body)["stores"]
     @stores = @first_stores + @second_stores
     @stores.take(15).each do |store|
-      OpenStruct.new(store)
+      new(store)
     end
+  end
+  
+  def self.service
+    BestBuyService.new
+  end
+  
+  def self.total(zip)
+    @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,name,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6&page=1")
+    @total = JSON.parse(@response.body)["total"]
   end
 end
