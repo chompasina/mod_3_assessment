73696d74e41826f34ebfb98edfdae8b9fde79355
Tommasina <tommasina1@gmail.com>
2016-09-20 09:31:51 -0600

    adds show method for one item and working on delete method

--- spec/features/api_consumer_can_view_items_spec.rb
+++ spec/features/api_consumer_can_view_items_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "API consumer views items" do
   scenario "they visit items index" do
     visit '/api/v1/items'
-    
+    require "pry"; binding.pry
     expect(page).to have_content("Items")
     
   end
