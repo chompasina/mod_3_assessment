ccf4274518ed198b209bb6d5d30a7320dacdd725
Tommasina <tommasina1@gmail.com>
2016-09-20 10:16:32 -0600

    making factories for tests

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,19 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   respond_to :JSON
   protect_from_forgery :except => :destroy
-#   When I send a GET request to `/api/v1/items`
-# I receive a 200 JSON response containing all items
-# And each item has an id, name, description, and image_url but not the created_at or updated_at
-# 
-# When I send a GET request to `/api/v1/items/1`
-# I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-# 
-# When I send a DELETE request to `/api/v1/items/1`
-# I receive a 204 JSON response if the record is successfully deleted
-# 
-# When I send a POST request to `/api/v1/items` with a name, description, and image_url
-# I receive a 201 JSON  response if the record is successfully created
-# And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+
   def index
     @items = Item.all
     
@@ -32,5 +20,12 @@ class Api::V1::ItemsController < ApplicationController
   end
   
   def create
-    
+    @item = Item.create(item_params)
+  end
+  
+  private
+  
+  def item_params
+    params.permit(:name, :description, :image_url)
+  end
 end
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe "User can see all items" do
   scenario "they visit items index" do
-    get '/api/v1/items'
+    #   When I send a GET request to `/api/v1/items`
+    # I receive a 200 JSON response containing all items
+    # And each item has an id, name, description, and image_url but not the created_at or updated_at
+    items = create(:item)
+    get 'api/v1/items'
     
     json = JSON.parse(response.body)
     
@@ -10,5 +14,25 @@ describe "User can see all items" do
     require "pry"; binding.pry
     expect(json['items'].length).to eq(500)
     
+  end
+  
+  scenario "they can see a specific item" do
+    # When I send a GET request to `/api/v1/items/1`
+    # I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+    # 
+  end
+  
+  scenario "they can delete an item" do
+    
+    # When I send a DELETE request to `/api/v1/items/1`
+    # I receive a 204 JSON response if the record is successfully deleted
+    # 
+  end
+  
+  scenario "they can create an item" do
+    # When I send a POST request to `/api/v1/items` with a name, description, and image_url
+    # I receive a 201 JSON  response if the record is successfully created
+    # And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+
   end
 end
